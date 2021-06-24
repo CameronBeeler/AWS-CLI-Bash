dbe6b13858ebd6756e43578905bacdfd0eade0d4
Cam Beeler <cameron.beeler@gmail.com>
2021-06-24 01:06:26 -0700

    update to include a new sso startup and to remove the sso login from getdns.  also, add bashlog.out log file

--- getdns.sh
+++ getdns.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+echo "GETDNS executed on $(date +%F-%T) ====================" >> bashlog.out
+
 # read the input file (account profile name)
 if [ $# -eq 0 ]
    then
    echo "Please enter a file with required AWS account profile names"
    read keep_going
-#   kill -INT $$
    return -1
 fi
 
 awsProfile=$1
-echo ${awsProfile}
+echo ${awsProfile} >> bashlog.out
 
 
 if [ -f "${awsProfile}" ]; then
-    echo "$awsProfile exists."
+    echo "$awsProfile exists.">> bashlog.out
 
 else 
     echo "Please enter a valid AWS profile input file"
@@ -22,30 +23,38 @@ else
     return -2
 fi
 
-echo "in the following steps, you will need to approve each profile CLI on your web browser before action can be taken"
-
 while read profile; 
 do 
-   aws sso login --profile $profile
    accountDomains=${profile}.domains
+   echo "reset the ${accountDomains} file to null" >> bashlog.out
    > ${accountDomains}
-echo ${accountDomains} domain file has been successfully created
+   echo ${accountDomains} domain file has been successfully created >> bashlog.out
 
    while read domainId ; 
    do
 
 #     read in the domain.  
-      hzFileName=${profile}.${domainId}
+      hzFileName=${profile}.${domainId}.rrs
+      echo "reset the ${hzFileName} file to null" >> bashlog.out
       > $hzFileName
-      echo ${hzFileName} >> ${accountDomains}
+      echo "-----adding ${hzFileName} to ${accountDomains}" >> bashlog.out
+      echo ${hzFileName} >> ${accountDomains} 
 
 
            while read hostedZone;
            do
+              echo "==== ${hostedZone} added to ${hzFileName}" >> bashlog.out
               echo ${hostedZone} >> ${hzFileName}
            done < <(aws route53 list-resource-record-sets --hosted-zone-id ${domainId} --output json --profile ${profile} |jq -jr '.ResourceRecordSets[] | "\(.Type) \t\(.Name) \t\(.ResourceRecords[]?.Value)\n"'|sort; exit)
     
       done < <(aws route53 list-hosted-zones --profile ${profile} --output json | jq '.[]|.[]|.Id'|sed 's:"/hostedzone/::'|sed 's:"$::'; exit)
 done < $awsProfile
 
-for nodomains in *.domains; do if [ ! -s ${nodomains} ]; then rm ${nodomains}; fi; done
+for nodomains in *.domains; 
+do 
+   if [ ! -s ${nodomains} ]; 
+   then 
+      echo "Remove the empty file ${nodomains}" >> bashlog.out
+      rm ${nodomains}; 
+   fi; 
+done
