0933cb49a8e3fb8cdc5446213c763b96c648cc24
Cam Beeler <cameron.beeler@gmail.com>
2021-06-23 21:27:01 -0700

    getdns is now fully functional and tested

--- getdns.sh
+++ getdns.sh
@@ -4,46 +4,46 @@
 if [ $# -eq 0 ]
    then
    echo "Please enter a file with required AWS account profile names"
-   exit -1
+   read keep_going
+#   kill -INT $$
+   return -1
 fi
 
-$AWS_Profile=$1
+awsProfile=$1
+echo ${awsProfile}
 
-if [ -f "$AWS_Profile" ]; then
-    echo "$AWS_Profile exists."
 
-    else 
+if [ -f "${awsProfile}" ]; then
+    echo "$awsProfile exists."
+
+else 
     echo "Please enter a valid AWS profile input file"
-    exit -2
+    read keep_going
+    return -2
 fi
 
 echo "in the following steps, you will need to approve each profile CLI on your web browser before action can be taken"
 
-while read -r profile; 
+while read profile; 
 do 
    aws sso login --profile $profile
-   echo your profile is $profile
-   echo "AFTER YOU HAVE APPROVED the SSO CLI auth, please press ENTER"
-   input trash_enter
-
-   if [ $trash_enter -eq 'exit' ]
-   then
-      exit -3
-   fi
-   
    accountDomains=${profile}.domains
    > ${accountDomains}
+echo ${accountDomains} domain file has been successfully created
 
    while read domainId ; 
    do
-#     read in the domain.  
-      echo ${profile}.${domainId} >> ${accountDomains}
 
-      while read hostedZone;
-      do
-         echo ${hostedZone} >> ${profile}.${domainId}
-         
-      done < <(aws route53 list-resource-record-sets --hosted-zone-id ${domianId} --output json --profile ${profile} |jq -jr '.ResourceRecordSets[] | "\(.Type) \t\(.Name) \t\(.ResourceRecords[]?.Value)\n"'|sort;exit)
+#     read in the domain.  
+      hzFileName=${profile}.${domainId}
+      > $hzFileName
+      echo ${hzFileName} >> ${accountDomains}
+
+
+           while read hostedZone;
+           do
+              echo ${hostedZone} >> ${hzFileName}
+           done < <(aws route53 list-resource-record-sets --hosted-zone-id ${domainId} --output json --profile ${profile} |jq -jr '.ResourceRecordSets[] | "\(.Type) \t\(.Name) \t\(.ResourceRecords[]?.Value)\n"'|sort; exit)
     
-   done < <(aws route53 list-hosted-zones --profile ${profile} --output json | jq '.[]|.[]|.Id'|sed 's:"/hostedzone/::'|sed 's:"$::'; exit)
-done < $AWS_profile
+      done < <(aws route53 list-hosted-zones --profile ${profile} --output json | jq '.[]|.[]|.Id'|sed 's:"/hostedzone/::'|sed 's:"$::'; exit)
+done < $awsProfile
