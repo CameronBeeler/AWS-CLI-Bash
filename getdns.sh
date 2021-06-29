1f9e41e5945c29cff04a4ceaa2c8f2b9e6aa063f
Cam Beeler <cameron.beeler@gmail.com>
2021-06-29 11:09:58 -0700

    update to add file counts and domain name and place in subdirectory

--- getdns.sh
+++ getdns.sh
@@ -2,11 +2,11 @@
 
 if [ ! -f bashlog.out ];
 then
-   echo "Create bashlog.out logfile from getdns shell, on $(date +%F-%T)" > bashlog.out
+   echo "$(date +%Y%m%d::%H%M) -  Create bashlog.out logfile from getdns shell" > bashlog.out
 fi
 
 
-echo "GETDNS executed on $(date +%F-%T) ====================" >> bashlog.out
+echo "$(date +%Y%m%d::%H%M) - GETDNS executed ====================" >> bashlog.out
 
 # read the input file (account profile name)
 if [ $# -eq 0 ]
@@ -16,12 +16,21 @@ if [ $# -eq 0 ]
    return -1
 fi
 
-awsProfile=$1
-echo "${awsProfile}" >> bashlog.out
+#
+# Create a final destination for all of the dns files
+#
+destination="./$(date +%Y%m%d_%H%M%S.dns_cleanup)/"
+mkdir ${destination}
+
+#
+# Name the sso input file
+#
+ssoProfileInputFile=$1
+echo "$(date +%Y%m%d::%H%M) -  ${ssoProfileInputFile}" >> bashlog.out
 
 
-if [ -f "${awsProfile}" ]; then
-    echo "$awsProfile exists.">> bashlog.out
+if [ -f "${ssoProfileInputFile}" ]; then
+    echo "$(date +%Y%m%d::%H%M) -  ${ssoProfileInputFile} exists.">> bashlog.out
 
 else 
     echo "Please enter a valid AWS profile input file"
@@ -29,41 +38,56 @@ else
     return -2
 fi
 
-while read profile; 
+while read ssoAccountProfileName; 
 do 
-   accountDomains=${profile}.domains
-   echo "reset the ${accountDomains} file to null" >> bashlog.out
-   > ${accountDomains}
-   echo "${accountDomains} domain file has been successfully created." >> bashlog.out
+   accountDomains=${ssoAccountProfileName}.domains
+   echo "$(date +%Y%m%d::%H%M) -  reset the ${accountDomains} file to null" >> bashlog.out
+   echo "$(date +%Y%m%d::%H%M) -  ${accountDomains} domain file has been successfully created." >> bashlog.out
 
    while read hostZoneInfo ; 
    do
 
+#
 #     read in the domain Id and the domain Name for file naming purposes.  
+#
       domainId=$(echo ${hostZoneInfo}|cut -d' ' -f1)
       domainName=$(echo ${hostZoneInfo}|cut -d' ' -f2|sed 's:.$::')
-      hzFileName=${profile}.$domainName.${domainId}.rrs
-      echo "reset the ${hzFileName} file to null." >> bashlog.out
-      > $hzFileName
-      echo "-----adding ${hzFileName} to ${accountDomains}." >> bashlog.out
+      hzFileName=${domainName}_${domainId}.rrs
+      echo "$(date +%Y%m%d::%H%M) -  reset the ${hzFileName} file to null." >> bashlog.out
+      > ${hzFileName}
+      echo "$(date +%Y%m%d::%H%M) -  -----adding ${hzFileName} to ${accountDomains}." >> bashlog.out
       echo ${hzFileName} >> ${accountDomains} 
 
 
            while read hostedZone;
            do
-              echo "==== ${hostedZone} added to ${hzFileName}." >> bashlog.out
+              echo "$(date +%Y%m%d::%H%M) -  ==== ${hostedZone} added to ${hzFileName}." >> bashlog.out
               echo ${hostedZone} >> ${hzFileName}
-           done < <(aws route53 list-resource-record-sets --hosted-zone-id ${domainId} --output json --profile ${profile} |jq -jr '.ResourceRecordSets[] | "\(.Type) \t\(.Name) \t\(.ResourceRecords[]?.Value)\n"'|sort; exit)
-    
-   done < <(aws route53 list-hosted-zones --profile ${awsProfile} --output json | jq -jr '.[] |.[]|"\(.Id) \t\(.Name)\n"'|sed 's:/hostedzone/::'; exit)
+           done < <(aws route53 list-resource-record-sets --hosted-zone-id ${domainId} --output json --profile ${ssoAccountProfileName} |jq -jr '.ResourceRecordSets[] | "\(.Type) \t\(.Name) \t\(.ResourceRecords[]?.Value)\n"'|sort; exit)
 
-done < $awsProfile
+    
+   done < <(aws route53 list-hosted-zones --profile ${ssoAccountProfileName} --output json | jq -jr '.[] |.[]|"\(.Id) \t\(.Name)\n"'|sed 's:/hostedzone/::'; exit)
+
+
+done < <(cat ${ssoProfileInputFile})
+
+# POST processing file manipulation and data massaging
 
 for nodomains in *.domains; 
 do 
    if [ ! -s ${nodomains} ]; 
    then 
-      echo "Remove the empty file ${nodomains}." >> bashlog.out
-      rm ${nodomains}; 
+      echo "$(date +%Y%m%d::%H%M) -  Remove the empty file ${nodomains}." >> bashlog.out
+      rm ${nodomains}  >> bashlog.out
    fi; 
 done
+
+for rrsFilename in *.rrs;
+do
+   echo "$(date +%Y%m%d::%H%M) - ${addRecordsToFilename} is the file to move to directory ${destination}" >> bashlog.out
+   mv ${rrsFilename} $(cat ${rrsFilename} | wc -l|sed 's:[ ]*::').${rrsFilename} >> bashlog.out
+done
+
+mv bashlog.out ${destination}. >> bashlog.out
+mv *.domains ${destination}. >> bashlog.out
+mv *.rrs ${destination}. >> bashlog.out
