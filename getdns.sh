91940f1156aedf96d05dbcef07912178aaa08def
Cam Beeler <cameron.beeler@gmail.com>
2021-07-02 16:56:44 -0700

    updating the specificity of dns to the bashlog

--- getdns.sh
+++ getdns.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ ! -f bashlog.out ];
+if [ ! -f bashlog_dns.out ];
 then
-   echo "$(date +%Y%m%d::%H%M) -  Create bashlog.out logfile from getdns shell" > bashlog.out
+   echo "$(date +%Y%m%d::%H%M) -  Create bashlog_dns.out logfile from getdns shell" > bashlog_dns.out
 fi
 
 
-echo "$(date +%Y%m%d::%H%M) - GETDNS executed ====================" >> bashlog.out
+echo "$(date +%Y%m%d::%H%M) - GETDNS executed ====================" >> bashlog_dns.out
 
 # read the input file (account profile name)
 if [ $# -eq 0 ]
@@ -26,11 +26,11 @@ mkdir ${destination}
 # Name the sso input file
 #
 ssoProfileInputFile=$1
-echo "$(date +%Y%m%d::%H%M) -  ${ssoProfileInputFile}" >> bashlog.out
+echo "$(date +%Y%m%d::%H%M) -  ${ssoProfileInputFile}" >> bashlog_dns.out
 
 
 if [ -f "${ssoProfileInputFile}" ]; then
-    echo "$(date +%Y%m%d::%H%M) -  ${ssoProfileInputFile} exists.">> bashlog.out
+    echo "$(date +%Y%m%d::%H%M) -  ${ssoProfileInputFile} exists.">> bashlog_dns.out
 
 else 
     echo "Please enter a valid AWS profile input file"
@@ -41,8 +41,8 @@ fi
 while read ssoAccountProfileName; 
 do 
    accountDomains=${ssoAccountProfileName}.domains
-   echo "$(date +%Y%m%d::%H%M) -  reset the ${accountDomains} file to null" >> bashlog.out
-   echo "$(date +%Y%m%d::%H%M) -  ${accountDomains} domain file has been successfully created." >> bashlog.out
+   echo "$(date +%Y%m%d::%H%M) -  reset the ${accountDomains} file to null" >> bashlog_dns.out
+   echo "$(date +%Y%m%d::%H%M) -  ${accountDomains} domain file has been successfully created." >> bashlog_dns.out
 
    while read hostZoneInfo ; 
    do
@@ -53,15 +53,15 @@ do
       domainId=$(echo ${hostZoneInfo}|cut -d' ' -f1)
       domainName=$(echo ${hostZoneInfo}|cut -d' ' -f2|sed 's:.$::')
       hzFileName=${domainName}_${domainId}.rrs
-      echo "$(date +%Y%m%d::%H%M) -  reset the ${hzFileName} file to null." >> bashlog.out
+      echo "$(date +%Y%m%d::%H%M) -  reset the ${hzFileName} file to null." >> bashlog_dns.out
       > ${hzFileName}
-      echo "$(date +%Y%m%d::%H%M) -  -----adding ${hzFileName} to ${accountDomains}." >> bashlog.out
+      echo "$(date +%Y%m%d::%H%M) -  -----adding ${hzFileName} to ${accountDomains}." >> bashlog_dns.out
       echo ${hzFileName} >> ${accountDomains} 
 
 
            while read hostedZone;
            do
-              echo "$(date +%Y%m%d::%H%M) -  ==== ${hostedZone} added to ${hzFileName}." >> bashlog.out
+              echo "$(date +%Y%m%d::%H%M) -  ==== ${hostedZone} added to ${hzFileName}." >> bashlog_dns.out
               echo ${hostedZone} >> ${hzFileName}
            done < <(aws route53 list-resource-record-sets --hosted-zone-id ${domainId} --output json --profile ${ssoAccountProfileName} |jq -jr '.ResourceRecordSets[] | "\(.Type) \t\(.Name) \t\(.ResourceRecords[]?.Value)\n"'|sort; exit)
 
@@ -77,17 +77,17 @@ for nodomains in *.domains;
 do 
    if [ ! -s ${nodomains} ]; 
    then 
-      echo "$(date +%Y%m%d::%H%M) -  Remove the empty file ${nodomains}." >> bashlog.out
-      rm ${nodomains}  >> bashlog.out
+      echo "$(date +%Y%m%d::%H%M) -  Remove the empty file ${nodomains}." >> bashlog_dns.out
+      rm ${nodomains}  >> bashlog_dns.out
    fi; 
 done
 
 for rrsFilename in *.rrs;
 do
-   echo "$(date +%Y%m%d::%H%M) - ${addRecordsToFilename} is the file to move to directory ${destination}" >> bashlog.out
-   mv ${rrsFilename} $(cat ${rrsFilename} | wc -l|sed 's:[ ]*::').${rrsFilename} >> bashlog.out
+   echo "$(date +%Y%m%d::%H%M) - ${addRecordsToFilename} is the file to move to directory ${destination}" >> bashlog_dns.out
+   mv ${rrsFilename} $(cat ${rrsFilename} | wc -l|sed 's:[ ]*::').${rrsFilename} >> bashlog_dns.out
 done
 
-mv bashlog.out ${destination}. >> bashlog.out
-mv *.domains ${destination}. >> bashlog.out
-mv *.rrs ${destination}. >> bashlog.out
+mv bashlog_dns.out ${destination}. >> bashlog_dns.out
+mv *.domains ${destination}. >> bashlog_dns.out
+mv *.rrs ${destination}. >> bashlog_dns.out
